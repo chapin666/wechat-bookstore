85ce46a7eaa457dc028cc820c9ce612030e97c28
ChengBin <Cheng-Bin@outlook.com>
2016-01-01 22:52:23 +0800

    2016-1-1 22:52

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -20,6 +20,9 @@ class IndexController extends BaseController {
 
 	   
            if($data && is_array($data)){
+
+		//记录微信推送过来的数据
+                file_put_contents('./data.json', json_encode($data));
 		//执行Demo
                 $this->demo($wechat, $data);
 	   }
@@ -105,5 +108,52 @@ class IndexController extends BaseController {
                 # code...
                 break;
         }
+    }
+
+
+ 	/**
+     * 资源文件上传方法
+     * @param  string $type 上传的资源类型
+     * @return string       媒体资源ID
+     */
+    private function upload($type){
+        $appid     = 'wx58aebef2023e68cd';
+        $appsecret = 'bf818ec2fb49c20a478bbefe9dc88c60';
+        $token = session("token");
+        if($token){
+            $auth = new WechatAuth($appid, $appsecret, $token);
+        } else {
+            $auth  = new WechatAuth($appid, $appsecret);
+            $token = $auth->getAccessToken();
+            session(array('expire' => $token['expires_in']));
+            session("token", $token['access_token']);
+        }
+        switch ($type) {
+            case 'image':
+                $filename = './Public/image.jpg';
+                $media    = $auth->materialAddMaterial($filename, $type);
+                break;
+            case 'voice':
+                $filename = './Public/voice.mp3';
+                $media    = $auth->materialAddMaterial($filename, $type);
+                break;
+            case 'video':
+                $filename    = './Public/video.mp4';
+                $discription = array('title' => '视频标题', 'introduction' => '视频描述');
+                $media       = $auth->materialAddMaterial($filename, $type, $discription);
+                break;
+            case 'thumb':
+                $filename = './Public/music.jpg';
+                $media    = $auth->materialAddMaterial($filename, $type);
+                break;
+            
+            default:
+                return '';
+        }
+        if($media["errcode"] == 42001){ //access_token expired
+            session("token", null);
+            $this->upload($type);
+        }
+        return $media['media_id'];
     }
 }
