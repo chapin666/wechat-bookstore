f4e27e1a9a9603b2dd7b21105e75a4309b243a9f
ChengBin <Cheng-Bin@outlook.com>
2016-01-01 23:28:26 +0800

    2016-1-1 23:28

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,157 +1,16 @@
 <?php
 namespace Home\Controller;
+use Home\Common\BaseController;
+use Home\Common\MessageUtils;
 
-use Think\Controller;
-use Com\Wechat;
-use Com\WechatAuth;
-
-class IndexController extends Controller {
+class IndexController extends BaseController {
 	
-	public function index($id='') {
-	   $appid = "wx8036d4cb719e2fa4";		//AppID(应用ID)
-           $crypt = "652789b17dac68885639fc48e47a2c6b"; //消息加密KEY（EncodingAESKey）
-	   $token = "z9fDzQv6F3dy6kylO9u4keS0uLO0gels7FUQ2RsFbn6"; //微信后台填写的TOKEN
-
-	   /* 加载微信SDK */
-           $wechat = new Wechat($token, $appid, $crypt);
-	   
-	   $data = $wechat->request();
-
-	   
-           if($data && is_array($data)){
-		//执行Demo
-                $this->demo($wechat, $data);
-	   }
+	public function index() {
+	    $wechatObj = new MessageUtils();
+	    if (isset($_GET['echostr'])) {
+		$wechatObj->valid();
+	    }else{
+		$wechatObj->responseMsg();
+	    }
 	}
-
-
-	  /**
-     * DEMO
-     * @param  Object $wechat Wechat对象
-     * @param  array  $data   接受到微信推送的消息
-     */
-    private function demo($wechat, $data){
-	echo "dsds";
-        switch ($data['MsgType']) {
-            case Wechat::MSG_TYPE_EVENT:
-                switch ($data['Event']) {
-                    case Wechat::MSG_EVENT_SUBSCRIBE:
-                        $wechat->replyText('欢迎您关注微书店公众平台！回复“文本”，“图片”，“语音”，“视频”，“音乐”，“图文”，“多图文”查看相应的信息！');
-                        break;
-                    case Wechat::MSG_EVENT_UNSUBSCRIBE:
-                        //取消关注，记录日志
-                        break;
-                    default:
-                        $wechat->replyText("欢迎访问微书店公众平台！您的事件类型：{$data['Event']}，EventKey：{$data['EventKey']}");
-                        break;
-                }
-                break;
-            case Wechat::MSG_TYPE_TEXT:
-                switch ($data['Content']) {
-                    case '文本':
-                        $wechat->replyText('欢迎访问微书店公众平台，这是文本回复的内容！');
-                        break;
-                    case '图片':
-                        //$media_id = $this->upload('image');
-                        $media_id = '1J03FqvqN_jWX6xe8F-VJr7QHVTQsJBS6x4uwKuzyLE';
-                        $wechat->replyImage($media_id);
-                        break;
-                    case '语音':
-                        //$media_id = $this->upload('voice');
-                        $media_id = '1J03FqvqN_jWX6xe8F-VJgisW3vE28MpNljNnUeD3Pc';
-                        $wechat->replyVoice($media_id);
-                        break;
-                    case '视频':
-                        //$media_id = $this->upload('video');
-                        $media_id = '1J03FqvqN_jWX6xe8F-VJn9Qv0O96rcQgITYPxEIXiQ';
-                        $wechat->replyVideo($media_id, '视频标题', '视频描述信息。。。');
-                        break;
-                    case '音乐':
-                        //$thumb_media_id = $this->upload('thumb');
-                        $thumb_media_id = '1J03FqvqN_jWX6xe8F-VJrjYzcBAhhglm48EhwNoBLA';
-                        $wechat->replyMusic(
-                            'Wakawaka!', 
-                            'Shakira - Waka Waka, MaxRNB - Your first R/Hiphop source', 
-                            'http://wechat.zjzit.cn/Public/music.mp3', 
-                            'http://wechat.zjzit.cn/Public/music.mp3', 
-                            $thumb_media_id
-                        ); //回复音乐消息
-                        break;
-                    case '图文':
-                        $wechat->replyNewsOnce(
-                            "全民创业蒙的就是你，来一盆冷水吧！",
-                            "全民创业已经如火如荼，然而创业是一个非常自我的过程，它是一种生活方式的选择。从外部的推动有助于提高创业的存活率，但是未必能够提高创新的成功率。第一次创业的人，至少90%以上都会以失败而告终。创业成功者大部分年龄在30岁到38岁之间，而且创业成功最高的概率是第三次创业。", 
-                            "http://www.topthink.com/topic/11991.html",
-                            "http://yun.topthink.com/Uploads/Editor/2015-07-30/55b991cad4c48.jpg"
-                        ); //回复单条图文消息
-                        break;
-                    case '多图文':
-                        $news = array(
-                            "全民创业蒙的就是你，来一盆冷水吧！",
-                            "全民创业已经如火如荼，然而创业是一个非常自我的过程，它是一种生活方式的选择。从外部的推动有助于提高创业的存活率，但是未必能够提高创新的成功率。第一次创业的人，至少90%以上都会以失败而告终。创业成功者大部分年龄在30岁到38岁之间，而且创业成功最高的概率是第三次创业。", 
-                            "http://www.topthink.com/topic/11991.html",
-                            "http://yun.topthink.com/Uploads/Editor/2015-07-30/55b991cad4c48.jpg"
-                        ); //回复单条图文消息
-                        $wechat->replyNews($news, $news, $news, $news, $news);
-                        break;
-                    
-                    default:
-                        $wechat->replyText("欢迎访问微书店公众平台！您输入的内容是：{$data['Content']}");
-                        break;
-                }
-                break;
-            
-            default:
-                # code...
-                break;
-        }
-    }
-
-
- /**
-     * 资源文件上传方法
-     * @param  string $type 上传的资源类型
-     * @return string       媒体资源ID
-     */
-    private function upload($type){
-        $appid     = 'wx8036d4cb719e2fa4';
-        $appsecret = '652789b17dac68885639fc48e47a2c6b';
-        $token = session("token");
-        if($token){
-            $auth = new WechatAuth($appid, $appsecret, $token);
-        } else {
-            $auth  = new WechatAuth($appid, $appsecret);
-            $token = $auth->getAccessToken();
-            session(array('expire' => $token['expires_in']));
-            session("token", $token['access_token']);
-        }
-        switch ($type) {
-            case 'image':
-                $filename = './Public/image.jpg';
-                $media    = $auth->materialAddMaterial($filename, $type);
-                break;
-            case 'voice':
-                $filename = './Public/voice.mp3';
-                $media    = $auth->materialAddMaterial($filename, $type);
-                break;
-            case 'video':
-                $filename    = './Public/video.mp4';
-                $discription = array('title' => '视频标题', 'introduction' => '视频描述');
-                $media       = $auth->materialAddMaterial($filename, $type, $discription);
-                break;
-            case 'thumb':
-                $filename = './Public/music.jpg';
-                $media    = $auth->materialAddMaterial($filename, $type);
-                break;
-            
-            default:
-                return '';
-        }
-        if($media["errcode"] == 42001){ //access_token expired
-            session("token", null);
-            $this->upload($type);
-        }
-        return $media['media_id'];
-    }
-
 }
