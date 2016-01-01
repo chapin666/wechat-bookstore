a477c135f6119caa96d50a0f3cd5727bdb579409
ChengBin <Cheng-Bin@outlook.com>
2016-01-01 16:38:41 +0800

    2016-1-1 16:38

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,6 +1,16 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use Home\Common\MessageUtils;
+
 class IndexController extends Controller {
-   
+	
+	public function index() {
+	    $wechatObj = new MessageUtils();
+	    if (isset($_GET['echostr'])) {
+		$wechatObj->valid();
+	    }else{
+		$wechatObj->responseMsg();
+	    }
+	}
 }
--- index.php
+++ index.php
@@ -20,7 +20,11 @@ define('APP_DEBUG',True);
 // 定义应用目录
 define('APP_PATH','./Application/');
 
-define('BIND_MODULE','Admin');
+
+// TOKEN
+define("TOKEN", "bookstore");
+
+//define('BIND_MODULE','Admin');
 
 
 // 引入ThinkPHP入口文件
