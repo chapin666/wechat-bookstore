301c541b5b5236f75d930815dbf1a2c73c553167
ChengBin <Cheng-Bin@outlook.com>
2016-01-02 23:41:48 +0800

    2016-1-2 23:41

---
+++ Application/Home/Common/Controller/BaseController.class.php
@@ -0,0 +1,78 @@
+<?php
+namespace Home\Common\Controller;
+
+use Think\Controller;
+use Vendor\Wechat\TPWechat;
+
+class BaseController extends Controller {
+	
+	private $options = array('token'=>'wsdbookstore', 'encodingaeskey'=>'tk3s8FIGe0MjpQyjT7eiMpwhsXHjzFZ5LrSEuoftIfn',
+ 					'appid'=>'wx9266bc9da8a1f391', 'appsecret'=>'');
+
+	private $menu = array ('button' => array (
+				0 => array ('name' => '浏览图书', 'sub_button' =>  array (
+				    0 => array (
+				      'type' => 'click',
+				      'name' => '最新上架',
+				      'key' => 'menu_0_0',
+				    ),
+				    1 => array (
+				      'type' => 'click',
+				      'name' => '热销书籍',
+				      'key' => 'menu_0_1',
+				    ),
+				    2 => array (
+				      'type' => 'click',
+				      'name' => '分类浏览',
+				      'key' => 'menu_0_2',
+				    ),
+				 ),
+				),
+
+				1 => array ('name' => '我的订单', 'sub_button' => array (
+				    0 => array (
+				      'type' => 'click',
+				      'name' => '购物车',
+				      'key' => 'menu_1_0',
+				    ),
+				    1 => array (
+				      'type' => 'click',
+				      'name' => '待付款',
+				      'key' => 'menu_1_1',
+				    )
+				    2 => array (
+				      'type' => 'click',
+				      'name' => '历史订单',
+				      'key' => 'menu_1_2',
+				    ),
+				),
+				),
+
+				2 => array ('name' => '个人中心', 'sub_button' => array (
+				    0 => array (
+				      'type' => 'click',
+				      'name' => '帐号绑定',
+				      'key' => 'menu_2_0',
+				    ),
+				    1 => array (
+				      'type' => 'click',
+				      'name' => '个人信息',
+				      'key' => 'menu_2_1',
+				    )
+				    2 => array (
+				      'type' => 'click',
+				      'name' => '解除绑定',
+				      'key' => 'menu_2_2',
+				    ),
+				),
+				),
+			);
+
+	protected $weChat = null;
+
+	public function __construct() {
+		$weChat = new TPWechat($this->options);
+		$weChat->valid();
+		$weChat->createMenu($this->menu);	
+	}
+}
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,66 +1,17 @@
 <?php
 namespace Home\Controller;
 
-use Think\Controller;
-use Vendor\Wechat\TPWechat;
+use Home\Common\Controller\BaseController;
 use Vendor\Wechat\Wechat;
 
-class IndexController extends Controller {
-	
-	 private $options = array(
-			'token'=>'wsdbookstore', //填写你设定的key
- 			'encodingaeskey'=>'tk3s8FIGe0MjpQyjT7eiMpwhsXHjzFZ5LrSEuoftIfn', //填写加密用的EncodingAESKey
- 			'appid'=>'wx9266bc9da8a1f391', //填写高级调用功能的app id
- 			'appsecret'=>'e5394eff409fbe82a7828ba0e8ce3aac' //填写高级调用功能的密钥
- 		);
-
-	private $menu = array ('button' => array (0 => array ('name' => '扫码',
-					'sub_button' => array (
-			     	            0 => array (
-			     	              'type' => 'scancode_waitmsg',
-			     	              'name' => '扫码带提示',
-			      	              'key' => 'rselfmenu_0_0',
-			      	            ),
-			      	            1 => array (
-			      	              'type' => 'scancode_push',
-			      	              'name' => '扫码推事件',
-			     	              'key' => 'rselfmenu_0_1',
-			     	            ),
-     	        			),
-      	      			),
-      	      			1 => array ('name' => '发图',
-		     	        'sub_button' => array (
-		      	            0 => array (
-		      	              'type' => 'pic_sysphoto',
-		      	              'name' => '系统拍照发图',
-		     	              'key' => 'rselfmenu_1_0',
-		      	            ),
-		      	            1 => array (
-		      	              'type' => 'pic_photo_or_album',
-		     	              'name' => '拍照或者相册发图',
-		      	              'key' => 'rselfmenu_1_1',
-		     	            )
-		     	        ),
-		     	      ),
-
-		     	      2 => array (
-		     	        'type' => 'location_select',
-		     	        'name' => '发送位置',
-		     	        'key' => 'rselfmenu_2_0'
-		     	      ),
-		      	    ),
-      	);
-
+class IndexController extends BaseController {
 
 
 	public function index() {
-		$weObj = new TPWechat($this->options);
-		$weObj->valid();
-		$weObj->createMenu($this->menu);
-		$type = $weObj->getRev()->getRevType();
+		$type = $weChat->getRev()->getRevType();
 		switch($type) {
 		    case Wechat::MSGTYPE_TEXT:
-			    $weObj->text("hello, I'm wechat")->reply();
+			    $weChat->text("hello, I'm wechat")->reply();
 			    exit;
 			    break;
 		    case Wechat::MSGTYPE_EVENT:
@@ -70,7 +21,7 @@ class IndexController extends Controller {
 				
 			    break;
 		    default:
-			    $weObj->text("help info")->reply();
+			    $weChat->text("help info")->reply();
 		}
 	}
 }
