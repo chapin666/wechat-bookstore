f9b6a2e7520beecbf67cb2cac0ff66db1ae7bbd1
chengbin <997155658@qq.com>
2016-03-24 02:45:42 +0800

    add etst

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,9 +2,17 @@
 return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'=>'mysql',// 数据库类型
-	'DB_HOST'=>'localhost',// 服务器地址
-	'DB_NAME'=>'bookstore',// 数据库名
-	'DB_USER'=>'root',// 用户名
-	'DB_PWD'=>'root',// 密码
-	'DB_PORT'=>3306,// 端口
+	'DB_HOST'=>'10.9.1.188',// 服务器地址
+	'DB_NAME'=>'cf_1ccc87bf_8a52_4bca_a46f_24b4b2f9da44',// 数据库名
+	'DB_USER'=>'JypeS58TzGwcA1la',// 用户名
+	'DB_PWD'=>'CSDJ3VsAO16PeiMg',// 密码
+	'DB_PORT'=>3306, // CSDJ3VsAO16PeiMg端口
+
+
+	// 'DB_TYPE'=>'mysql',// 数据库类型
+	// 'DB_HOST'=>'localhost',// 服务器地址
+	// 'DB_NAME'=>'bookstore',// 数据库名
+	// 'DB_USER'=>'root',// 用户名
+	// 'DB_PWD'=>'root',// 密码
+	// 'DB_PORT'=>3306,// 端口
 );
