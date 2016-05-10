98e4c8338c632b21da50a2620dd0f01e66d60a64
Cheng Bin <cheng-bin@outlook.com>
2016-05-10 18:00:40 +0800

    test

---
+++ test.js
@@ -0,0 +1,20 @@
+var BookStoreCookie = {
+	
+	Day: 7 * 24 * 60 * 60 * 1000,
+	Now: new Date(),
+	Name: "book",
+
+	save: function(bookId) {
+		alert(this.Name);
+	},
+
+
+	get: function() {
+
+	},
+
+
+	delete: function() {
+
+	}
+};
